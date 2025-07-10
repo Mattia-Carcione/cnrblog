75210aaecda9c97ce990c304a1b0f47cf6bae348
Mattia Carcione <mattia.carcione01@gmail.com>
2025-07-10 20:56:56 +0200

    add footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,30 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+
+/* SITE */
 export const SITE_TITLE = 'Blog di Anna';
+export const COMPANY_NAME = 'commenti.non.richiesti';
 export const SITE_DESCRIPTION = 'Scopri luoghi originali. Recensioni vere, esperienze autentiche e pratiche mini-guide.';
+
+/* SOCIAL */
 export const INSTA_SITE = 'https://www.instagram.com/commenti.non.richiesti?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==';
 export const TIKTOK_SITE = 'https://www.instagram.com/commenti.non.richiesti?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==';
 export const PAYPAL = 'https://www.paypal.me/AnnaSantovitoCNR';
 export const MAIL = 'test@gmail.com';
+
+/* UTILS */
 export const IMG_PLACEHOLDER = 'https://placehold.co/400x400';
+export const NAV_LINKS = [
+  { label: "Home", href: "/" },
+  { label: "Blog", href: "/blog" },
+  { label: "Mini-Guide", href: "/guide" },
+  { label: "Chi Sono", href: "/about" },
+  { label: "Servizi", href: "/servizi" },
+];
+
+export const SOCIAL_LINKS = [
+  { href: INSTA_SITE, icon: "instagram", label: "Vai al mio sito Instagram", title: "Instagram di Anna", target: "_blank", rel: "noopener noreferrer", },
+  { href: TIKTOK_SITE, icon: "tiktok", label: "Vai al mio sito TikTok", title: "TikTok di Anna", target: "_blank", rel: "noopener noreferrer", },
+  { href: `mailto:${MAIL}`, icon: "envelope", label: "Contattami per email", title: "Mail di Anna", },
+];
